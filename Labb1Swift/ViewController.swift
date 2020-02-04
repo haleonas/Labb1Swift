110116e8de62cf03d4d76ca7ab98d93a2ee0161f
haleonas <altes@live.se>
2020-02-04 12:46:46 +0100

    Initial Commit

---
+++ Labb1Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Labb1Swift
+//
+//  Created by Jesper Stenlund on 2020-02-04.
+//  Copyright © 2020 Jesper Stenlund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
